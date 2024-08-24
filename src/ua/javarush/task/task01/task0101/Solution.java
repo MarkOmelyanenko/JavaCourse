12f9e2b86740f5a08d6e29a090cc52c67e3746e7
MarkOmelyanenko <famil.mark@gmail.com>
2024-08-24 21:05:27 +0200

    console print, var types, comments

---
+++ src/ua/javarush/task/task01/task0101/Solution.java
@@ -0,0 +1,7 @@
+package ua.javarush.task.task01.task0101;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("Я думаю, бути програмістом - круто");
+    }
+}
