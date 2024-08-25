d5caa91f3b90670ed3a3a4307175c09fc1f44bc7
MarkOmelyanenko <famil.mark@gmail.com>
2024-08-25 23:23:53 +0200

    exceptions

---
+++ src/ua/javarush/task/pro/task14/task1409/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task14.task1409;
+
+public class Solution {
+
+    public static void main(String[] args) throws Exception {
+        generateLuckyNumber();
+    }
+
+    static void generateLuckyNumber() throws Exception {
+        int luckyNumber = (int) (Math.random() * 100);
+        if (luckyNumber == 13) {
+            throw new Exception("не пощастило");
+        }
+        System.out.println("твоє щасливе число: " + luckyNumber);
+    }
+}
