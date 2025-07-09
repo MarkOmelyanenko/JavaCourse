635db417af9c07cf0744417993e4b3229f0a6f4a
MarkOmelyanenko <famil.mark@gmail.com>
2025-07-09 23:38:56 +0200

    flow chains

---
+++ src/ua/javarush/task/pro/task15/task1507/Solution.java
@@ -0,0 +1,22 @@
+package ua.javarush.task.pro.task15.task1507;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+
+        try(Scanner scanner = new Scanner(System.in);)
+        {
+            List<String> lines = Files.readAllLines(Paths.get(scanner.nextLine()));
+
+            for (int i = 0; i < lines.size(); i += 2) {
+                System.out.println(lines.get(i));
+            }
+        }
+    }
+}
+
