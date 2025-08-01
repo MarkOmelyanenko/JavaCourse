6f51d04784f14b0d4e5b4c2acce85443d97a8435
MarkOmelyanenko <famil.mark@gmail.com>
2025-08-02 01:22:30 +0200

    Path

---
+++ src/ua/javarush/task/pro/task15/task1513/Solution.java
@@ -0,0 +1,16 @@
+package ua.javarush.task.pro.task15.task1513;
+
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+
+        Path path = Path.of(str).getRoot();
+
+        System.out.println(path);
+    }
+}
+
