017e9c87025b11c3fa0f5021b6228b4a01e1e3d0
MarkOmelyanenko <famil.mark@gmail.com>
2025-08-14 14:29:24 +0200

    add Functional Interfaces, add Stream

---
+++ src/ua/javarush/task/pro/task18/task1808/Solution.java
@@ -0,0 +1,17 @@
+package ua.javarush.task.pro.task18.task1808;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Solution {
+    public static void main(String[] args) {
+        var strings = new ArrayList<String>();
+        Collections.addAll(strings, "Hello", "Amigo", "JavaRush", "CodeGym");
+
+        print(strings);
+    }
+
+    public static void print(ArrayList<String> strings) {
+        strings.forEach(System.out::println);
+    }
+}
