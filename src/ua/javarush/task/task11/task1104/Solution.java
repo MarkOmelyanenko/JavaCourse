003fb9cc2c8db3f4dd5386300270164adca780da
MarkOmelyanenko <famil.mark@gmail.com>
2025-08-17 22:16:46 +0200

    add Inheritance

---
+++ src/ua/javarush/task/task11/task1104/Solution.java
@@ -0,0 +1,22 @@
+package ua.javarush.task.task11.task1104;
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
